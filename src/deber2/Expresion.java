6cb1b7e84533ea1f26d7cd860d9dfa77c3c21b91
gbernal096 <gbernal096@gmail.com>
2015-12-01 23:42:03 -0500

    Deber2

--- src/deber2/Expresion.java
+++ src/deber2/Expresion.java
@@ -1,10 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this license header, choose License Headers in Project Prtmpties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package deber2;
 
+import static java.lang.Math.pow;
 import java.util.LinkedList;
 
 /**
@@ -13,26 +14,82 @@ import java.util.LinkedList;
  */
 class Expresion {
 
+    /**
+     * convierte ede InFix a PostFix
+     * @param expresion expresión a convertir
+     * @return String de la expresión en PostFix
+     */
     static String InFixToPostFix(String expresion) {
-        if(expresion == null || expresion.isEmpty())
+        if (expresion == null || expresion.isEmpty()) {
             return null;
+        }
         String postFix = "";
         LinkedList<Character> charList = toList(expresion);
-        for(Character c : charList){
-            
+        LinkedList<Character> pila = new LinkedList<>();
+        for (Character c : charList) {
+            if (c == '(') {
+                pila.push('(');
+            } else if (c == ')') {
+                Character tmp = pila.peek();
+                while (tmp !='(' && !pila.isEmpty()) {
+                    postFix += tmp;
+                    pila.pop();
+                    tmp = pila.peek();
+                }
+                pila.pop();
+            } else if (c == '+' || c == '-') {
+                //Si la pila está vacía, recuerda la operación.
+                if (pila.isEmpty()) {
+                    pila.push(c);
+                } else {
+                    Character tmp = pila.peek();
+                    while (!(pila.isEmpty() || tmp =='(' || tmp == ')')) {
+                        pila.pop();
+                        postFix += tmp;
+                    }
+                    pila.push(c);
+                }
+            } else if (c == '*' || c == '/') {
+                if (pila.isEmpty()) {
+                    pila.push(c);
+                } else {
+                    Character tmp = pila.peek();
+                    while (!pila.isEmpty() && tmp != '+' && tmp != '-' ) {
+                        pila.pop();
+                        postFix += tmp;
+                    }
+                    pila.push(c);
+                }
+            } else {
+                postFix += c;
+            }
+        }
+        //Agrego los operadores restantes a la expresión
+        while (!pila.isEmpty()) {
+            Character tmp = pila.peek();
+            if (tmp != '(') {
+                pila.pop();
+                postFix += tmp.charValue();
+            }
         }
         return postFix;
     }
 
+    /**
+     * verifica los paréntesis de una expresión
+     * @param expresion expresión a verificar
+     * @return true si la expresión tiene los paréntesis correctos.
+     */
     static boolean verificaParentesis(String expresion) {
-        if(expresion == null || expresion.isEmpty())
+        if (expresion == null || expresion.isEmpty()) {
             return false;
-        LinkedList<Character> charList = toList(expresion);
+        }
         LinkedList<Character> cpar = new LinkedList<>();
-        for (Character c : charList) {
-            if (c.charValue() == '(') {
+        for (int i = 0 ; i<expresion.length();i++) {
+            char c = expresion.charAt(i);
+            if (c == '(') {
                 cpar.push(c);
-            } else if (c.charValue() == ')') {
+            } else if (c == ')') {
                 if (cpar.isEmpty()) {
                     return false;
                 } else {
@@ -40,10 +97,15 @@ class Expresion {
                 }
             }
         }
-        return charList.isEmpty();
+        return cpar.isEmpty();
     }
 
-    static LinkedList<Character> toList(String expresion){
+    /**
+     * método auxiliar para transformar la expresión en una lista
+     * @param expresion expresión a transformar
+     * @return lista de caracteress 
+    */
+    static LinkedList<Character> toList(String expresion) {
         LinkedList<Character> charList = new LinkedList<>();
         for (int i = 0; i < expresion.length(); i++) {
             char x = expresion.charAt(i);
@@ -52,8 +114,107 @@ class Expresion {
         return charList;
     }
 
+    /**
+     * evalúa una expresión en PostFix
+     * @param expresion expresión a evaluar
+     * @param variables variables de la expresión
+     * @return resultado como String
+     */
     static String evaluar(String expresion, LinkedList<Variable> variables) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        LinkedList<Integer> tmp = new LinkedList<>();
+        for (Character c : toList(expresion)) {
+            if (Character.isLetter(c)) {
+                try {
+                    tmp.push(getValor(c, variables));
+                    continue;
+                } catch (Exception ex) {
+                    return "Variable " + c + " no definida.";
+                }
+            }
+            if (c == '+') {
+                if (tmp.size() < 2) {
+                    return "Expresion mal formada";
+                }
+                int b = tmp.pop();
+                int a = tmp.pop();
+                tmp.push(a + b);
+                continue;
+            }
+            if (c == '^') {
+                if (tmp.size() < 2) {
+                    return "Expresion mal formada";
+                }
+                int b = tmp.pop();
+                int a = tmp.pop();
+                tmp.push((int) pow(a, b));
+                continue;
+            }
+            if (c == '-') {
+                if (tmp.size() < 2) {
+                    return "Expresion mal formada";
+                }
+
+                int b = tmp.pop();
+                int a = tmp.pop();
+                tmp.push(a - b);
+                continue;
+            }
+            if (c == '*') {
+                if (tmp.size() < 2) {
+                    return "Expresion mal formada";
+                }
+
+                int b = tmp.pop();
+                int a = tmp.pop();
+                tmp.push(a * b);
+                continue;
+            }
+            if (c == '/') {
+                if (tmp.size() < 2) {
+                    return "Expresion mal formada";
+                }
+
+                int b = tmp.pop();
+                int a = tmp.pop();
+                tmp.push(a / b);
+                continue;
+            }
+        }
+        return tmp.size() != 1 ? "Expresión malformada" : tmp.pop() + "";
+    }
+
+    /**
+     * retorna el valor de una variable dada
+     * @param nvar nombre de varialbe
+     * @param vars lista de variables
+     * @return el valor de la variable buscada por su nombre
+     * @throws Exception en caso de que la variable no exista en la lista de variables.
+     */
+    static int getValor(Character nvar, LinkedList<Variable> vars) throws Exception {
+        Variable target = new Variable(nvar, 0);
+        for (Variable var : vars) {
+            if (var.equals(target)) {
+                return var.valor;
+            }
+        }
+        throw new Exception("Variable no existe");
+    }
+    
+    /**
+     * devuelve las variables de una expresión
+     * @param expresion expresión de la cual se extraen las variables
+     * @return lista de variables
+     */
+    static LinkedList<Variable> obtenerVariables(String expresion) {
+        LinkedList<Variable> vars = new LinkedList<>();
+        for (Character c : toList(expresion)) {
+            Variable foo = new Variable(c,0); //variable temporal que representa el caracter
+            if (Character.isLetter(c) && !vars.contains(foo)) {
+                vars.add(foo);
+            }
+            c.equals('c');
+        }
+        return vars;
     }
 
 }
--- src/deber2/UsoPilas.java
+++ src/deber2/UsoPilas.java
@@ -6,7 +6,11 @@
 package deber2;
 
 import java.util.LinkedList;
+import javax.print.attribute.AttributeSet;
 import javax.swing.JOptionPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
 
 /**
  *
@@ -19,7 +23,7 @@ public class UsoPilas {
         LinkedList<Variable> variables = new LinkedList<>();
 
         // Solicitar la expresiòn en in-fixpor teclado
-        JOptionPane.showInputDialog("Ingrese expresión a evaluar: ", expresion);
+        expresion = JOptionPane.showInputDialog(null, "Ingrese expresión a verificar: ");
         if (Expresion.verificaParentesis(expresion)) {
             System.out.println("La expresion es correcta");
         } else {
@@ -28,8 +32,32 @@ public class UsoPilas {
         expresion = Expresion.InFixToPostFix(expresion);
         System.out.println("Expresion en post-fix: " + expresion);
 
-               // Solicitar la expresiòn en post-fix por teclado
+        // Solicitar la expresiòn en post-fix por teclado
+        expresion = "";
+        expresion = JOptionPane.showInputDialog(null, "Ingrese expresión en Post-Fix a evaluar: ");
         // solicitar las variables por teclado
+        if (expresion == null || expresion.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Expresión vacía, cerrando...");
+            return;
+        }
+
+        variables = Expresion.obtenerVariables(expresion);
+        for (Variable var : variables) {
+            //http://stackoverflow.com/questions/23398718/converting-joptionpane-input-to-integer
+            String errorMessage = "";
+            do {
+                // Show input dialog with current error message, if any
+                String stringInput = JOptionPane.showInputDialog(errorMessage + "Ingresa el valor de "+var.nombre+".");
+                try {
+                    var.valor = Integer.parseInt(stringInput);
+                   errorMessage ="";
+                } catch (NumberFormatException e) {
+                    // The typed text was not an integer
+                    errorMessage = "The text you typed is not a number.\n";
+                }
+            } while (!errorMessage.isEmpty());
+        }
+
         System.out.println("El resultado es: " + Expresion.evaluar(expresion, variables));
     }
 }
@@ -43,4 +71,27 @@ class Variable {
         this.nombre = nombre;
         this.valor = valor;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + this.nombre;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Variable other = (Variable) obj;
+        if (this.nombre != other.nombre) {
+            return false;
+        }
+        return true;
+    }
+
 }
